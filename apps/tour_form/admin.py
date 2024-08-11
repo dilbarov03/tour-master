eadb46f009c469c55dfe3bcf46e8502de3648882
dilbarov03 <dilbarov03@gmail.com>
2024-08-12 00:06:44 +0500

    add tour offer inline

--- apps/tour_form/admin.py
+++ apps/tour_form/admin.py
@@ -40,15 +40,56 @@ class TourPeopleInline(admin.TabularInline):
     extra = 0
 
 
+class TourOfferInline(admin.StackedInline):
+    model = TourOffer
+    extra = 0
+    readonly_fields = ["answered_by", "status"]
+
+    def save_new_objects(self, request, form, inline_formset):
+        new_objects = []
+        for form in inline_formset.forms:
+            if form.instance.pk is None:
+                form.instance.answered_by = request.user
+                new_objects.append(form.instance)
+        return new_objects
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
 @admin.register(TourForm)
 class TourFormAdmin(ImportExportModelAdmin, ExportActionMixin):
     list_display = ["id", "user", "region", "tour_type", "country", "from_date", "to_date", "phone"]
     list_filter = ["user", "region", "tour_type", "country"]
     list_per_page = 25
-    inlines = [TourPeopleInline]
-    readonly_fields = ["answered_at"]
-
+    inlines = [TourPeopleInline, TourOfferInline]
+    readonly_fields = ["answered_at", ]
     resource_class = TourFormResource
+
+    def save_formset(self, request, form, formset, change):
+        new_objects = []
+        if formset.model == TourOffer:
+            for inline_form in formset.forms:
+                obj = inline_form.save(commit=False)
+                if not obj.pk:  # New object, not yet saved
+                    obj.answered_by = request.user
+                    obj.save()  # Save to the database
+                    new_objects.append(obj)
+
+        # Save the rest of the formset
+        formset.save()
+
+        # Send notification for each new TourOffer object
+        for obj in new_objects:
+            channel_layer = get_channel_layer()
+            async_to_sync(channel_layer.group_send)(
+                f"user_{obj.tour_form.user.id}",
+                {
+                    "type": "notify",
+                    "event": "New Tour Offer",
+                    "message": f"A new tour offer has been created by {request.user.id}"
+                }
+            )
 
 
 @admin.register(TourOffer)
