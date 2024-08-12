144dd7b26c6862601528f9b570b1d95cb2fe0b58
root <dilbarov03@gmail.com>
2024-08-12 10:01:05 +0200

    prod settings

--- core/settings/production.py
+++ core/settings/production.py
@@ -13,9 +13,11 @@ DEBUG = True
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
-CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
 CSRF_TRUSTED_ORIGINS = [
-    "https://63f7-62-169-31-254.ngrok-free.app/"
+    "https://63f7-62-169-31-254.ngrok-free.app/",
+    "http://62.169.31.254/"
 ]
 
 
