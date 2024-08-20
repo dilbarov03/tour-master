e4a05644ded3260fb4138d70a0ec0693f8f62cf9
Uktamjon <dilbarov03@gmail.com>
2024-08-20 11:59:01 +0500

    change excel export

--- apps/tour_catalog/resources.py
+++ apps/tour_catalog/resources.py
@@ -4,23 +4,30 @@ from .models import UserBooking
 
 
 class UserBookingResource(resources.ModelResource):
-    user = fields.Field(column_name='Пользователь', attribute='user')
+    user = fields.Field(column_name='Sotuvchi', attribute='user')
+    is_bought = fields.Field(column_name='Sotib olingan', attribute='is_bought')
 
     class Meta:
         model = UserBooking
-        fields = ('id', 'user', 'region__name', 'full_name', 'tg_username', 'phone',
+        fields = ('id', 'user', 'branch__name', 'region__name', 'full_name', 'tg_username', 'phone',
                   'tour__name', 'tour__category__name', 'tour__start_date',
-                  'tour__barcode', 'total_price', 'created_at')
+                  'tour__barcode', 'total_price', 'is_bought', 'created_at')
 
     def dehydrate_user(self, user_booking):
         return user_booking.user.get_userinfo()
+
+    def dehydrate_is_bought(self, user_booking):
+        if user_booking.is_bought:
+            return "Ha"
+        return "Yo'q"
 
     def export(self, queryset=None, **kwargs):
         dataset = super().export(queryset=queryset, **kwargs)
 
         dataset.headers = [
-            "Zayavka ID", "Sotuvchi", "Hudud", "Mijoz", "Telegramdagi foydalanuvchi nomi", "Telefon",
-            "Tur nomi", "Sayohat turi", "Boshlanish sanasi", "Shtrix kod", "Umumiy narxi", "Yaratilgan vaqti"
+            "Zayavka ID", "Sotuvchi", "Filial", "Hudud", "Mijoz", "Telegramdagi foydalanuvchi nomi", "Telefon",
+            "Tur nomi", "Sayohat turi", "Boshlanish sanasi", "Shtrix kod", "Umumiy narxi", "Sotib olingan",
+            "Yaratilgan vaqti"
         ]
 
         return dataset
